c892bcbe38e69d8219e3b282188a1af46c3cdf24
Mousssous <76653576+Mousssous@users.noreply.github.com>
2023-07-20 17:18:48 +0200

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,1 +1,5 @@
+'''translator module containing englishToFrench and frenchToEnglish functions'''
+import translator
 
+print(translator.english_to_french("Hello"))
+print(translator.french_to_english("Bonjour"))
