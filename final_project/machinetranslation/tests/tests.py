400aee75930dd732de661fd5bfb8797b1f9ec1d1
Mousssous <76653576+Mousssous@users.noreply.github.com>
2023-07-21 16:14:11 +0200

    Update tests.py

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,5 +1,21 @@
 '''translator module containing englishToFrench and frenchToEnglish functions'''
 import translator
+import unittest
 
-print(translator.english_to_french("Hello"))
-print(translator.french_to_english("Bonjour"))
+class TestStringMethods(unittest.TestCase):
+
+    def test_english_to_french(self):
+        self.assertEqual(translator.english_to_french("bread"), "pain")
+        self.assertEqual(translator.english_to_french("bed"), "lit")
+
+        self.assertNotEqual(translator.english_to_french("printer"), "lampe")
+        self.assertNotEqual(translator.english_to_french("container"), "souris")
+
+    def test_french_to_english(self):
+        self.assertEqual(translator.french_to_english("pain"), "bread")
+        self.assertEqual(translator.french_to_english("lit"), "bed")
+
+        self.assertNotEqual(translator.french_to_english("imprimante"), "mouse")
+        self.assertNotEqual(translator.french_to_english("conteneur"), "lamp")
+
+unittest.main()
